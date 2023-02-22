7168ffae0075f36b7ef956a08418e894c0ac95da
Huy <s3825059@rmit.edu.vn>
2023-02-22 23:58:57 +0700

    finish form UI

--- client/src/types.ts
+++ client/src/types.ts
@@ -2,3 +2,18 @@ export interface INavBarItemProps {
   title: string;
   classprops?: any;
 }
+
+export interface IInputProps {
+  placeholder: string,
+  name: string,
+  type: string,
+  value: string,
+  handleChange: (e: any, name: string) => void
+}
+
+export interface IFormData {
+  addressTo: string,
+  amount: string,
+  keyword: string,
+  message: string
+}
